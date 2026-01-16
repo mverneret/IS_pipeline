69407aed35177fe297d6e6d25f2753f1655fb421
mverneret <74653572+mverneret@users.noreply.github.com>
2026-01-16 08:49:49 +0100

    Update commands.sh
    Signed-off-by: mverneret <74653572+mverneret@users.noreply.github.com>

--- IS_simulation/files/commands.sh
+++ IS_simulation/files/commands.sh
@@ -1,1 +1,9 @@
-
+####### Simulate IS and reads #######
+bash /home/ubuntu/data/mydatalocal/IS_pipeline/IS_simulation/scripts/simulate_reads.sh \
+  -r /home/ubuntu/data/mydatalocal/IS_pipeline/test/ref/ARS12_noscaffold_masked.fa \
+  -n 1000 \
+  -f 260 \
+  -l /home/ubuntu/data/mydatalocal/IS_pipeline/IS_simulation/data/seq_LTR_linker_CH.fasta \
+  -d random_IS_CH_1k \
+  -o /home/ubuntu/data/mydatalocal/IS_pipeline/IS_simulation/sim \
+  > /home/ubuntu/data/mydatalocal/IS_pipeline/IS_simulation/random_IS_CH_1k.log 2>&1
