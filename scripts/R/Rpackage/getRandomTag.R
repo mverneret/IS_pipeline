5e8a6309dd917fb783052b2fd0c2eb07a725418c
mverneret <74653572+mverneret@users.noreply.github.com>
2026-01-22 16:13:57 +0100

    Update getRandomTag.R
    Signed-off-by: mverneret <74653572+mverneret@users.noreply.github.com>

--- scripts/R/Rpackage/getRandomTag.R
+++ scripts/R/Rpackage/getRandomTag.R
@@ -5,36 +5,34 @@ getRandomTag <- function(fasta_file) {
   # Define a function to compute the reverse complement of a DNA sequence
   reverse_complement <- function(sequence) {
     # Replace bases with their complements and reverse the sequence
-    complement <- chartr("ACGTacgt", "TGCAtgca", sequence)
-    paste(rev(strsplit(complement, "")[[1]]), collapse = "")
+    complement <- chartr("ACGT", "TGCA", sequence)
+    return(paste(rev(unlist(strsplit(complement, ""))), collapse = ""))
   }
   
   # Define a function to parse each header line and extract information
-  parse_fasta_header <- function(header) {
+  parse_fasta_header <- function(header, apply_reverse_complement) {
     # Extract the sequence name, umi_fwd_seq, and umi_rev_seq using regex
-    readID <- str_match(header, ">([^;]+)")[, 2]
+    name <- str_match(header, ">([^;]+)")[, 2]
     umi_fwd_seq <- str_match(header, "umi_fwd_seq=([A-Za-z]*)")[, 2]
     umi_rev_seq <- str_match(header, "umi_rev_seq=([A-Za-z]*)")[, 2]
     
-    umi_fwd_seq <- ifelse(umi_fwd_seq %in% c("", "None"), NA, umi_fwd_seq)
-    umi_rev_seq <- ifelse(umi_rev_seq %in% c("", "None"), NA, umi_rev_seq)
-
-    # Decide which UMI to use
-    if (!is.na(umi_fwd_seq)) {
-      umi <- umi_fwd_seq
-    } else if (!is.na(umi_rev_seq)) {
-      umi <- reverse_complement(umi_rev_seq)
+    # If apply_reverse_complement is TRUE, take the reverse complement of umi_rev_seq
+    umi <- if (apply_reverse_complement) {
+      reverse_complement(umi_rev_seq)
     } else {
-      umi <- NA
+      umi_fwd_seq
     }
     
     # Return a data frame with extracted information
     data.frame(
-      readID = readID,
-      Sequence = umi,
+      readID = name,
+      Sequence = ifelse(umi == "", NA, umi),
       stringsAsFactors = FALSE
     )
   }
+  
+  # Determine if reverse complement should be applied based on the filename
+  apply_reverse_complement <- grepl("LTR3", fasta_file)
   
   # Read the FASTA file
   fasta_lines <- readLines(fasta_file)
@@ -43,7 +41,7 @@ getRandomTag <- function(fasta_file) {
   headers <- fasta_lines[startsWith(fasta_lines, ">")]
   
   # Parse each header and combine into a single data frame
-  randomTag <- do.call(rbind, lapply(headers, parse_fasta_header))
+  randomTag <- do.call(rbind, lapply(headers, parse_fasta_header, apply_reverse_complement))
   
   # Apply additional sequence modifications
   randomTag$Sequence <- gsub("^TTT|TTT$", "", randomTag$Sequence)
@@ -51,3 +49,4 @@ getRandomTag <- function(fasta_file) {
   
   return(randomTag)
 }
+
